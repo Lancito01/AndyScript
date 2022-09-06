496cda3f8d32639e12f3b70f95e080186bf6c470
Andy <68301335+Lancito01@users.noreply.github.com>
2022-09-06 01:01:43 -0300

    Update andyscript.lua

--- andyscript.lua
+++ andyscript.lua
@@ -1,10 +1,9 @@
-script_version = "v0.0.10"
-util.require_natives(1651208000)
+script_version = "v0.0.11"
+util.require_natives(1660775568)
 util.keep_running()
 
 local store = filesystem.store_dir()
 local shortcut_path = store .. "/shortcuts.txt"
-local shortcut_open_file = io.open(shortcut_path, "r")
 
 local notif_prefix = "[AndyScript] "
 local og_toast = util.toast
@@ -31,6 +30,25 @@ local function announce(string)
         util.toast(string)
     end
 end
+
+local function request_model(hash, timeout)
+    local end_time = os.time() + (timeout or 5)
+    STREAMING.REQUEST_MODEL(hash)
+    while not STREAMING.HAS_MODEL_LOADED(hash) and end_time >= os.time() do
+        util.yield()
+    end
+    return STREAMING.HAS_MODEL_LOADED(hash)
+end
+
+local some_ped_list = {
+    "a_m_m_bevhills_02",    --1
+    "a_m_m_business_01",    --2
+    "a_m_m_bevhills_01",    --3
+    "a_m_m_farmer_01",      --4
+    "a_m_m_paparazzi_01",   --5
+    "a_m_m_prolhost_01",    --6
+    "a_m_m_stlat_02"        --7
+}
 
 local function get_vehicle_ped_is_in(ped, includeLastVehicle)
     if includeLastVehicle or PED.IS_PED_IN_ANY_VEHICLE(ped, false) then
@@ -67,112 +85,112 @@ local info_tab = menu.list(menu.my_root(), "About")
 --Self tab
 --Godmode
 menu.toggle(self_tab, "Godmode", {"andygodmode"}, "Toggles several Stand features such as Godmode, Gracefulness, and Vehicle Godmode all at the same time to make you invincible against mortals.", 
-    function(state)
-        local switch_for_godmode = state and "On" or "Off"
-        menu.trigger_command(menu.ref_by_path("Self>Immortality", 38), switch_for_godmode)
-        menu.trigger_command(menu.ref_by_path("Self>Gracefulness", 38), switch_for_godmode)
-        menu.trigger_command(menu.ref_by_path("Self>Auto Heal", 38), switch_for_godmode)
-        menu.trigger_command(menu.ref_by_path("Vehicle>Indestructible", 38), switch_for_godmode)
-        menu.trigger_command(menu.ref_by_path("Self>Glued To Seats", 38), switch_for_godmode)
-        announce("Godmode " .. switch_for_godmode)
-    end
+function(state)
+    local switch_for_godmode = state and "On" or "Off"
+    menu.trigger_command(menu.ref_by_path("Self>Immortality", 38), switch_for_godmode)
+    menu.trigger_command(menu.ref_by_path("Self>Gracefulness", 38), switch_for_godmode)
+    menu.trigger_command(menu.ref_by_path("Self>Auto Heal", 38), switch_for_godmode)
+    menu.trigger_command(menu.ref_by_path("Vehicle>Indestructible", 38), switch_for_godmode)
+    menu.trigger_command(menu.ref_by_path("Self>Glued To Seats", 38), switch_for_godmode)
+    announce("Godmode " .. switch_for_godmode)
+end
 )
 
 --Ghost
 menu.toggle(self_tab, "Ghost", {"andyghostmode"}, "Toggles several Stand features such as Invisibility and Off The Radar all at the same time to make you fully invisible.",
-    function(state)
-        menu.trigger_command(menu.ref_by_path("Self>Appearance>Invisibility>" .. (state and "Enabled" or "Disabled"), 38))
-        menu.set_value(menu.ref_by_path("Online>Off The Radar", 38), state)
-        announce("Ghostmode " .. (state and "On" or "Off"))
-    end
+function(state)
+    menu.trigger_command(menu.ref_by_path("Self>Appearance>Invisibility>" .. (state and "Enabled" or "Disabled"), 38))
+    menu.set_value(menu.ref_by_path("Online>Off The Radar", 38), state)
+    announce("Ghostmode " .. (state and "On" or "Off"))
+end
 )
 
 --Semigodmode
 menu.toggle_loop(self_tab, "Heal Loop", {"healloop"}, "",
-    function()
-        if ENTITY.GET_ENTITY_HEALTH(players.user_ped()) ~= 0 then
-            local max_health = ENTITY.GET_ENTITY_MAX_HEALTH(players.user_ped())
-            ENTITY.SET_ENTITY_HEALTH(players.user_ped(), max_health, 0)
-        end
+function()
+    if ENTITY.GET_ENTITY_HEALTH(players.user_ped()) ~= 0 then
+        local max_health = ENTITY.GET_ENTITY_MAX_HEALTH(players.user_ped())
+        ENTITY.SET_ENTITY_HEALTH(players.user_ped(), max_health, 0)
     end
+end
 )
 
 --Heal
 menu.action(self_tab, "Max Health", {"healself"}, "Heals your ped to its max health.",
-    function()
-        local max_health = ENTITY.GET_ENTITY_MAX_HEALTH(players.user_ped())
-        ENTITY.SET_ENTITY_HEALTH(players.user_ped(), max_health, 0)
-        announce("Health maxed")
-    end
+function()
+    local max_health = ENTITY.GET_ENTITY_MAX_HEALTH(players.user_ped())
+    ENTITY.SET_ENTITY_HEALTH(players.user_ped(), max_health, 0)
+    announce("Health maxed")
+end
 )
 
 --Max armor
 menu.action(self_tab, "Max Armor", {}, "Maxes out your armor.",
-    function()
-        PED.SET_PED_ARMOUR(players.user_ped(), 100)
-    end
+function()
+    PED.SET_PED_ARMOUR(players.user_ped(), 100)
+end
 )
 
 --Clean
 menu.action(self_tab, "Clean", {"cleanself"}, "Cleans your ped from all visible blood.",
-    function()
-        PED.CLEAR_PED_BLOOD_DAMAGE(players.user_ped())
-        announce("Ped cleaned")
-    end
+function()
+    PED.CLEAR_PED_BLOOD_DAMAGE(players.user_ped())
+    announce("Ped cleaned")
+end
 )
 
 --Online tab
 --Weapons
 menu.toggle(online_tab, "Give All Weapons After Joining A Session", {}, "As soon as the transition is over, get all weapons.",
-    function(state)
-        give_weapons_after_transition = state
-    end
+function(state)
+    give_weapons_after_transition = state
+end
 )
 
 --Popularity loop
 local popularity_loop_command_ref = menu.ref_by_path("Online>Quick Progress>Set Nightclub Popularity", 38)
 menu.toggle_loop(online_tab, "Nightclub Popularity Loop", {"ncpopularityloop"}, "Toggles the Nightclub popularity loop to always keep it at 100%",
-    function()
-        menu.trigger_command(popularity_loop_command_ref, 100)
-        util.toast("Popularity set")
-        util.yield(2000)
-    end
+function()
+    menu.trigger_command(popularity_loop_command_ref, 100)
+    util.toast("Popularity set")
+    util.yield(2000)
+end
 )
 
 --Transition
 menu.toggle(online_tab, "Notification When Transition Is Over", {"notifyontransitionend"}, "Toasts a notification when the main transition is over.",
-    function(state)
-        announce_transition_end = state
-    end
+function(state)
+    announce_transition_end = state
+end
 )
 
 --Vehicles tab
 --Radio off automatically
 local last_vehicle_with_radio_off = 0
 menu.toggle_loop(vehicles_tab, "Turn Off Radio Automatically", {}, "Turns off the radio each time you get in a vehicle",
-    function()
-        local current_vehicle = get_vehicle_ped_is_in(players.user_ped())
-        if current_vehicle ~= 0 then
-            if last_vehicle_with_radio_off ~= current_vehicle and VEHICLE.GET_IS_VEHICLE_ENGINE_RUNNING(current_vehicle) then
-                if AUDIO._IS_VEHICLE_RADIO_ENABLED(current_vehicle) then
-                    util.yield(1000)
-                    AUDIO.SET_RADIO_TO_STATION_NAME("OFF")
-                    announce("Radio off")
-                end
-                last_vehicle_with_radio_off = current_vehicle
+function()
+    local current_vehicle = get_vehicle_ped_is_in(players.user_ped())
+    if current_vehicle ~= 0 then
+        if last_vehicle_with_radio_off ~= current_vehicle and VEHICLE.GET_IS_VEHICLE_ENGINE_RUNNING(current_vehicle) then
+            if AUDIO._IS_VEHICLE_RADIO_ENABLED(current_vehicle) then
+                util.yield(1000)
+                AUDIO.SET_RADIO_TO_STATION_NAME("OFF")
+                announce("Radio off")
             end
-        else
-            last_vehicle_with_radio_off = 0
+            last_vehicle_with_radio_off = current_vehicle
         end
+    else
+        last_vehicle_with_radio_off = 0
     end
+end
 )
 
 --Settings
 --Announce actions
 menu.toggle(settings_tab, "Announce Actions", {}, 'Announces every action done by the script, i.e. "Health maxed". This could also be called "Debug mode" (?',
-    function(state)
-        announce_actions = state
-    end
+function(state)
+    announce_actions = state
+end
 )
 
 --Shortcuts
@@ -195,6 +213,7 @@ local function create_shortcut(shortcut_title, shortcut_new, shortcut_old)
     else
         table.insert(shortcuts, {new_button, shortcut_new, shortcut_title, shortcut_old})
     end
+    write_to_shortcut_file(io.open(shortcut_path, "w"))
 end
 
 local function import_shortcuts_from_file(shortcut_new, shortcut_name, shortcut_old)
@@ -203,19 +222,21 @@ end
 
 local function read_shortcut_file(filehandle)
     local what_line_am_i_reading = 0
+    local imported_shortcut_count = 0
     for line in filehandle:lines() do
         what_line_am_i_reading += 1
         local data = string.split(line, ",") ---@diagnostic disable-line
-        if data[1] ~= "" and data[2] ~= "" and data[3] ~= "" then
+        if data[2] ~= "" and data[3] ~= "" then
             import_shortcuts_from_file(data[1], data[2], data[3])
+            imported_shortcut_count += 1
         else
             util.toast("Error reading from shortcuts file at line " .. what_line_am_i_reading .. ".")
         end
     end
-    util.toast("Imported " .. what_line_am_i_reading .. " previous shortcuts.")
+    util.toast("Imported " .. imported_shortcut_count .. " previous shortcuts.")
 end
 
-local function write_to_shortcut_file(filehandle)
+function write_to_shortcut_file(filehandle)
     for index, value in ipairs(shortcuts) do
         if value[1] ~= -1 then
             if type(value[4]) ~= "table" then
@@ -235,6 +256,7 @@ local function write_to_shortcut_file(filehandle)
             end
         end
     end
+    filehandle:flush()
 end
 
 shortcut_menu = menu.list(settings_tab, "Andy's Shortcuts")
@@ -248,7 +270,8 @@ menu.toggle(shortcut_menu, "Enable", {"andyshortcuts"}, "Enable " .. '"' .. "And
 function(state)
     shortcut_status = state
     if not have_shortcuts_been_enabled then
-        read_shortcut_file(shortcut_open_file)
+        local shortcut_open_file_read = io.open(shortcut_path, "r") -- Open file in read-only mode
+        read_shortcut_file(shortcut_open_file_read)
         have_shortcuts_been_enabled = true
     end
     --To hide the buttons before you enable it
@@ -265,11 +288,13 @@ function(state)
         local shortcut_maker_subtitle = menu.divider(single_custom_shortcut_menu, "Once you're done, press this:")
         local create_shortcut_command = menu.action(single_custom_shortcut_menu, "Create", {}, "Creates the custom shortcut with the given specifications.",
         function()
-            if pcall(menu.ref_by_command_name, menu.get_value(new_shortcut_command)) then
+            if not pcall(menu.ref_by_command_name, menu.get_value(new_shortcut_command)) then
+                util.toast("Error creating shortcut. Is the original command given correct?")
+            elseif menu.get_value(new_shortcut_title) == "" then
+                util.toast("Error creating shortcut. Does it have a name?")
+            else
                 create_shortcut(menu.get_value(new_shortcut_title), menu.get_value(new_shortcut), menu.get_value(new_shortcut_command))
                 util.toast('New shortcut "' .. menu.get_value(new_shortcut_title) .. '" created!')
-            else
-                util.toast("Error creating shortcut. Is the original command given correct?")
             end
         end)
 
@@ -282,12 +307,14 @@ function(state)
         local multiple_shortcut_maker_subtitle = menu.divider(multiple_custom_shortcut_menu, "Once you're done, press this:")
         local multiple_create_shortcut_command = menu.action(multiple_custom_shortcut_menu, "Create", {}, "Creates the custom shortcut with the given specifications.",
         function()
-            if pcall(menu.ref_by_command_name, menu.get_value(multiple_custom_command1)) and pcall(menu.ref_by_command_name, menu.get_value(multiple_custom_command2)) then
+            if not pcall(menu.ref_by_command_name, menu.get_value(multiple_custom_command1)) and pcall(menu.ref_by_command_name, menu.get_value(multiple_custom_command2)) then
+                util.toast("Error creating shortcut. Are the original commands given correct?")
+            elseif multiple_new_shortcut_title == "" then
+                util.toast("Error creating shortcut. Does it have a name?")
+            else
                 local multiple_og_commands = menu.get_value(multiple_custom_command1) .. ";" .. menu.get_value(multiple_custom_command2)
                 create_shortcut(menu.get_value(multiple_new_shortcut_title), menu.get_value(multiple_new_shortcut), multiple_og_commands)
                 util.toast('New shortcut "' .. menu.get_value(multiple_new_shortcut_title) .. '" created!')
-            else
-                util.toast("Error creating shortcut. Are the original commands given correct?")
             end
         end)
 
@@ -297,13 +324,20 @@ function(state)
         local remover_subtitle = menu.divider(remove_a_shortcut, "Once you're done, press this:")
         local remove_button = menu.action(remove_a_shortcut, "Remove", {}, "Removes the inputted shortcut from the entry and your shortcut file.", 
             function()
-                for k, v in shortcuts do
-                    if v[2] == menu.get_value(remover_shortcut) then
-                        menu.delete(v[1])
-                        v[1] = 0
-                        table.remove(shortcuts, k)
-                        util.toast('Shortcut "' .. menu.get_value(remover_shortcut) .. '" removed.')
+                if shortcuts[1] then
+                    for k, v in shortcuts do
+                        if v[2] == menu.get_value(remover_shortcut) then
+                            menu.delete(v[1])
+                            v[1] = 0
+                            table.remove(shortcuts, k)
+                            write_to_shortcut_file(io.open(shortcut_path, "w"))
+                            util.toast('Shortcut "' .. menu.get_value(remover_shortcut) .. '" removed.')
+                        else
+                            util.toast("Shortcut not found.")
+                        end
                     end
+                else
+                    util.toast("There are no shortcuts. Try creating one before deleting!")
                 end
             end)
 
@@ -350,28 +384,91 @@ menu.readonly(credits_under_info_tab, "Ren", "For helping me with majority of th
 --Player root
 --Check for modder
 local function is_player_modder(pid)
-    local suffix
-    menu.divider(menu.player_root(pid), "AndyScript")
-    menu.action(menu.player_root(pid), "Check If Player Is Modder", {"isplayermodder"}, "Checks if the selected player is a modder, then displays the result in local chat. Other players can't see this message.",
-        function()
-            suffix = players.is_marked_as_modder(pid) and " is a modder" or " is not a modder"
-            chat.send_message(players.get_name(pid) .. suffix,
-            true, -- is team chat
-            true, -- is in local history
-            false -- is networked
-            )
-        end
+    local suffix = players.is_marked_as_modder(pid) and " has set off modder detections." or " hasn't set off modder detections."
+    chat.send_message(players.get_name(pid) .. suffix,
+    true, -- is team chat
+    true, -- is in local history
+    false -- is networked
     )
 end
 
+--local function pipe_bomb(pid)
+--    menu.action(menu.player_root(pid), "Sends A Pipebomb Directly To Their Mailbox", {}, "Tracks their house based on their IP and geolocation data and sends an untraceable pipebomb to their home and places it stealthily inside their mailbox for a funny surprise.", function()
+--    util.toast("We're out of pipebombs. The fuck are you doin anyways") end)
+--end
+
+--Spawn ped in car
+local function spawn_ped_in_car(car, playerped, isclone)
+    if car == 0 then
+        util.toast("Player is not in a car.")
+    elseif VEHICLE.ARE_ANY_VEHICLE_SEATS_FREE(car) then
+        if isclone then
+            local ped_created = PED.CLONE_PED(playerped, true, false, true)
+            local seat = VEHICLE.IS_VEHICLE_SEAT_FREE(car, -1, false) and -1 or -2
+            PED.SET_PED_INTO_VEHICLE(ped_created, car, seat)
+        else
+            local coords = ENTITY.GET_ENTITY_COORDS(car, false)
+            local random_ped = some_ped_list[math.random(#some_ped_list)]
+            local random_ped_hash = util.joaat(random_ped)
+            request_model(random_ped_hash) -- Ped is now loaded in memory, so i don't care about what it returns. Point of this is to load ped before i spawn it (otherwise ped won't spawn)
+            local ped_created = entities.create_ped(4, random_ped_hash, coords, 0)
+            local seat = VEHICLE.IS_VEHICLE_SEAT_FREE(car, -1, false) and -1 or -2
+            PED.SET_PED_INTO_VEHICLE(ped_created, car, seat)
+        end
+    else
+        util.toast("There are no available seats free in their car.")
+    end
+end
+
+local function fill_car_with_peds(vehicle, playerped, isclone)
+    if vehicle == 0 then
+        util.toast("Player is not in a car.")
+    elseif VEHICLE.ARE_ANY_VEHICLE_SEATS_FREE(vehicle) then
+        while VEHICLE.ARE_ANY_VEHICLE_SEATS_FREE(vehicle) do
+            spawn_ped_in_car(vehicle, playerped, isclone)
+        end
+    else
+        util.toast("There are no available seats free in their car.")
+    end
+end
+
 local function is_player_andy(pid)
-    if players.get_rockstar_id(pid) == 99063679 or players.get_rockstar_id(pid) == 183828684 or players.get_rockstar_id(pid) == 181860426 or players.get_rockstar_id(pid) == 114010022 or players.get_rockstar_id(pid) == 35827130 then
+    if players.get_rockstar_id(pid) == 35827130 then
         util.toast("'" .. players.get_name(pid) .. "'" .. " (AndyScript dev) is joining. Say hi!")
     end
 end
 
+local function generate_features(pid)
+    menu.divider(menu.player_root(pid), "AndyScript")
+    local vehicles_player_root = menu.list(menu.player_root(pid), "Vehicles")
+    local online_player_root = menu.list(menu.player_root(pid), "Online")
+    menu.toggle(vehicles_player_root, "Include Player's Last Vehicle", {}, "Include the selected player's last vehicle in the vehicle-related functions.", function(state) include_last_vehicle_for_player_functions = state end)
+    menu.action_slider(vehicles_player_root, "Clone Ped Inside Their Car", {}, "Clones the player's ped and places it in the first free seat it finds.", {"Once", "Fill vehicle"},
+        function(index)
+            local player_ped = PLAYER.GET_PLAYER_PED(pid)
+            local car = get_vehicle_ped_is_in(player_ped, include_last_vehicle_for_player_functions) -- alternatively: local car = use_last_vehicle_toggle and PED.GET_VEHICLE_PED_IS_IN(ped) or get_vehicle_ped_is_in(pid, false)        
+            if index == 1 then
+                spawn_ped_in_car(car, player_ped, true)
+            elseif index == 2 then
+                fill_car_with_peds(car, player_ped, true)
+            end
+        end)
+        menu.action_slider(vehicles_player_root, "Spawn Random Ped Inside Their Car", {}, "Spawns a random ped from our (not very) extensive list and places it on the first available seat it finds.", {"Once", "Fill vehicle"},
+        function(index)
+            local player_ped = PLAYER.GET_PLAYER_PED(pid)
+            local car = get_vehicle_ped_is_in(player_ped, include_last_vehicle_for_player_functions) -- alternatively: local car = use_last_vehicle_toggle and PED.GET_VEHICLE_PED_IS_IN(ped) or get_vehicle_ped_is_in(pid, false)        
+            if index == 1 then
+                spawn_ped_in_car(car, player_ped, false)
+            elseif index == 2 then
+                fill_car_with_peds(car, player_ped, false)
+            end
+        end)
+    menu.action(online_player_root, "Check If Player Is Modder", {"isplayermodder"}, "Checks if the selected player is a modder, then displays the result in local chat. Other players can't see this message.", function() is_player_modder(pid) end)
+    --pipe_bomb(pid)
+end
+
 local function on_player_join(pid)
-    is_player_modder(pid)
+    generate_features(pid)
     is_player_andy(pid)
 end
 
